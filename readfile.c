b2062dd9715e7ea678b06014bd3e6a66f311a2bd
root <root@debian-BULLSEYE-live-builder-AMD64>
2023-04-10 16:28:30 +0300

    Added readfile function

---
+++ readfile.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
+int read_file_to_fd(char *filename, int file_fd, int fd);
+
+// буфер для считываемых символов
+char buf[BUFSIZ];
+
+int main(int argc, char *argv[]) {
+	int file_fd, bytes;
+	int res = 0;
+	
+	for (int i = 1; i < argc; i++) {
+		// открываем файл для чтения
+		file_fd = open(argv[i], O_RDONLY, 0);
+		
+		if (file_fd == -1) {
+			fprintf(stderr, "%s: %s\n", argv[i], strerror(errno));
+		} else {
+			// получаем кол-во записанных байт
+			if ((bytes = read_file_to_fd(argv[i], file_fd, 1)) != -1) {
+				res += bytes;
+			}
+			// закрываем файд
+			close(file_fd);
+		}
+	}
+	printf("read %d bytes\n", res);
+
+	return 0;
+}
+
+int read_file_to_fd(char *filename, int file_fd, int fd) {
+	int n, w;
+
+	// читаем файл в буфер
+	n = read(file_fd, buf, BUFSIZ);
+	if (n == -1) {
+		fprintf(stderr, "%s: %s\n", filename, strerror(errno));
+		return -1;
+	}
+
+	// записываем в fd прочитанные байты
+	w = write(fd, buf, n);
+	if (w == -1) {
+		fprintf(stderr, "%s: %s\n", filename, strerror(errno));
+		return -1;
+	}
+
+	return w;
+}
