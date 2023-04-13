69c635f514eb3c7b4393cb09e1ddb3772de6d755
root <root@debian-BULLSEYE-live-builder-AMD64>
2023-04-13 17:44:56 +0300

    up parsinghttp

--- HTTP/parsinghttp.c
+++ HTTP/parsinghttp.c
@@ -6,11 +6,8 @@
 
 // структура заголовков
 struct Headers {
-	char *host;
-	char *connection;
-	char *user_agent;
-	char *date;
-	char *lg;	
+	char *key;
+	char *value;
 };
 
 // структура запроса
@@ -18,18 +15,25 @@ struct Http_request {
 	char *method;
 	char *url;
 	char *protocol_version;
-	struct Headers *headers;	
+	struct Headers **headers;	
 };
 
-void readreq(int fd);
+void readreq(int fd, struct Http_request *http);
 
 int main(int argc, char *argv[]) {
+
+	struct Http_request *http;
+	http = (struct Http_request *) malloc(sizeof(struct Http_request));
+	if (http == NULL) {
+		perror("malloc");
+		return 1;
+	}
 
 	if (argc == 2) {
 		// открыть файл
 		int fd = open(argv[1], O_RDONLY, 0);
 		// прочитать файл
-		readreq(fd);
+		readreq(fd, http);
 		// закрыть файл
 		close(fd);
 	}
@@ -37,13 +41,13 @@ int main(int argc, char *argv[]) {
 	return 0;
 }
 
-void readreq(int fd) {
+void readreq(int fd, struct Http_request *http) {
 
 	// буфер для посимвольного чтения файла 
-	char buf[1];
-	int n, size, count;
+	char c;
+	int n, size, count, i;
 	size = 5;
-	count = 0;
+	count = i = 0;
 
 	// выделяем память под строку файла
 	char *strp;
@@ -54,7 +58,7 @@ void readreq(int fd) {
 	}
 
 	// пока не конец файла
-	while ((n = read(fd, buf, 1)) != 0) {
+	while ((n = read(fd, &c, 1)) != 0) {
 
 		// если выделенная память заполнена, расширяем ее размер
 		if (count == size) {
@@ -64,18 +68,35 @@ void readreq(int fd) {
 				perror("realloc");
 				return;
 			}
-
 		}
 
 		// если символ не \n
-		if (*buf != '\n') {
-			// инкрементируем счетчик записанных в память байт
-			++count;
-			// добавляем в выделенную под строку память копию каждого не \n символа
-			strcat(strp, strdup(buf));
+		if (c != '\n') {
+			// добавляем в выделенную под строку память значение каждого не \n символа
+			strp[count++] = c;
 		} else {
-			printf("%s\n", strp);
+			strp[count++] = '\0';
+			for (; i < count; i++) {
+				if (strp[i] == ' ') {
+					strp[i] = '\0';
+				} 
+			}	
 		}
 	}
+
+	int start = 0;
+	http->method = &strp[start];
+	
+	while (strp[start] != '\0') {
+		++start;
+	}
+	http->url = &strp[++start];
+	while (strp[start] != '\0') {
+		++start;
+	}
+	http->protocol_version = &strp[++start];
+		++start;
+
+	printf("method: %s\nurl: %s\nver: %s\n", http->method, http->url, http->protocol_version);
 
 }
