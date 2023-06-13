95349550b362c5262bc635f96de26ed929898148
root <root@debian-BULLSEYE-live-builder-AMD64>
2023-06-13 20:53:22 +0300

    Added check '*'

--- labyrinth/labyrinth.c
+++ labyrinth/labyrinth.c
@@ -18,10 +18,11 @@ struct Labyrinth {
 };
 
 int parse(struct Labyrinth *, int fd, int file_size);
-char *get_line(int fd, int size, int num_line);
+char *get_row(struct Labyrinth *, int fd, int size, int num_line);
 void print_lab(struct Labyrinth);
 char **create_arr(struct Labyrinth *, int fd, int file_size);
 void destroy_lab(struct Labyrinth *);
+void get_point(struct Labyrinth *, int c, int num_line, int num_c);
 
 int main(int argc, char *argv[]) {
 	struct Labyrinth lab;
@@ -113,7 +114,7 @@ int parse(struct Labyrinth *lab, int fd, int file_size) {
 	char *line;
 	
 	// читаю первую строку файла
-	if ((line = get_line(fd, 10, 1)) == NULL) {
+	if ((line = get_row(lab, fd, 10, 1)) == NULL) {
 		return -1;
 	}
 	// записываю размер лабиринта
@@ -141,10 +142,11 @@ char **create_arr(struct Labyrinth *lab, int fd, int file_size) {
 	}
 
 	int i;
+	int count = 0;
 	// формирую двумерный массив
 	for (i = 0; i < lab->size; i++) {
 		// читаю строку из файла
-		line = get_line(fd, lab->size, i + 2);
+		line = get_row(lab, fd, lab->size, i + 2);
 		
 		// проверка строки на EOF: файл меньше заданной размерности
 		if (line == NULL) {
@@ -157,16 +159,19 @@ char **create_arr(struct Labyrinth *lab, int fd, int file_size) {
 		// {p, p, p, p}: p -> {'a', 'b', 'c'}
 		lab->labyrinth[i] = line;
 
-		// получить координаты '*'
-		for (int k = 0; k < lab->size; k++) {
-			if (line[k] == '*') {
-				lab->point.x = i + 2;
-				lab->point.y = k + 1;
-			}
-		}
+		// подсчет '*' в строках
+		if (lab->point.x == i + 2) {
+			++count;
+		}	
 	}
-	// проверка, что координаты '*' получены
-	if (lab->point.x == 0) {
+	// проверка, что '*' один на весь файл 
+	if (count > 1) {
+		fprintf(stderr, "file incorrect: %d symbol '*' in file\n", count);
+		destroy_lab(lab);
+		return NULL;
+	}
+	// проверка, что координаты '*' получены 
+	if (!count) {
 		fprintf(stderr, "file incorrect: missing symbol '*'\n");
 		destroy_lab(lab);
 		return NULL;
@@ -175,7 +180,7 @@ char **create_arr(struct Labyrinth *lab, int fd, int file_size) {
 	// позиция в файле после прочтения
 	int pos = lseek(fd, 0, SEEK_CUR);
 	if (pos == -1) {
-		fprintf(stderr, "%s\n", strerror(errno));
+		perror("lseek");
 	}
 	if (pos != file_size) {
 		fprintf(stderr, "file size is larger than matrix size\n");
@@ -186,15 +191,15 @@ char **create_arr(struct Labyrinth *lab, int fd, int file_size) {
 	return lab->labyrinth;
 }
 
-char *get_line(int fd, int size, int num_line) {
+char *get_row(struct Labyrinth *lab, int fd, int size, int num_line) {
 	char *s = (char *) calloc(size, sizeof(char));
 	if (s == NULL) {
 		perror("calloc");
 		return NULL;
 	}
 	
-	int i, c;
-	i = c = 0;
+	int i, c, count;
+	i = c = count = 0;
 	char buf = 0;
 	
 	// читать по символу из файла в отдельный буфер
@@ -213,6 +218,19 @@ char *get_line(int fd, int size, int num_line) {
 			return NULL;
 		}				
 		s[i++] = buf;
+		
+		// получить координаты '*'
+		get_point(lab, buf, num_line, i);
+		
+		// подсчет символов '*' в одной строке
+		if (buf == '*') {
+			++count;
+		}
+	}
+	// проверка, что в строке один символ '*' или ни одного
+	if (count > 1) {
+		fprintf(stderr, "file incorrect: %d symbol '*' in %d line\n", count, num_line);
+		return NULL;
 	}
 
 	if (c == 0) {
@@ -225,4 +243,11 @@ char *get_line(int fd, int size, int num_line) {
 	}
 	
 	return s;
+}
+
+void get_point(struct Labyrinth *lab, int c, int num_line, int num_c) {
+	if (c == '*') {
+		lab->point.x = num_line;
+		lab->point.y = num_c;
+	}
 }
