1866b2d882c8b35642b5f8296f6ee837d1565282
user <nastya@debian-BULLSEYE-live-builder-AMD64>
2023-06-28 15:26:56 +0300

    Add merge_sort

---
+++ sort/merge_sort.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#define LEN(x) (int) (sizeof(x) / sizeof(x[0]))
+
+// 						 *8	[13, 3, 8, 1, 15, 2, 3, 7, 4]
+// 							  0  1  2  3 |4|  5  6  7  8
+// 					 *4	[13, 3, 8, 1, 15]  *7 [2, 3, 7, 4]
+//                        0  1 |2| 3   4       5 |6|  7  8
+//              *2 [13, 3, 8] *3 [1, 15]    *5 [2, 3] *6 [7, 4]
+//                   0 |1| 2     |3|  4        |5| 6     |7| 8
+//            *1 [13, 3]  [8]   [1]  [15]      [2]  [3]  [7]  [4] 
+//                |0| 1    2     3     4        5    6    7    8
+//             [13] [3]
+//               0   1
+
+void merge_sort(int *arr, int low, int high);
+void merge(int *arr, int low, int high, int mid);
+int main(void) {
+
+	int arr[] = {13, 3, 8, 1, 15, 2, 3, 7, 4};
+	int low = 0;
+	int high = LEN(arr) - 1;
+	for (int i = 0; i < LEN(arr); i++) {
+		printf("%d\t", arr[i]);
+	}
+	putchar('\n');
+	merge_sort(arr, low, high);
+	return 0;
+}
+
+void merge_sort(int *arr, int low, int high) {
+	// в сортируемом массиве должно быть больше одного элемента
+	if (low == high) {
+		return;
+	}
+	// находим индекс среднего элемента
+	int mid = low + (high - low) / 2;
+	// разбиваем массив на подмассивы, пока не дойдем до 1 элемента в массиве (в сортировке не нуждается)
+	merge_sort(arr, low, mid);
+	merge_sort(arr, mid + 1, high);
+	printf("low: %d mid: %d high: %d\n", arr[low], mid, arr[high]);
+	// сортируем и заменяем в исходном массиве отсортированным блоком элементов исходный блок
+	merge(arr, low, high, mid);
+	for (int i = low; i <= high; i++) {
+		printf("%d\t", arr[i]);
+	}
+	putchar('\n');
+}
+
+void merge(int *arr, int low, int high, int mid) {
+	// создать массив для отсортированных элементов
+	int len = 0;
+	for (int i = low; i <= high; i++) {
+		++len;
+	}
+	char buf[len];
+
+	// сортировка
+	int i, j, k;
+	i = low;
+	j = mid + 1;
+	k = 0;
+	// пока в двух половинах есть элементы 
+	while (i <= mid && j <= high) {
+		// сравниваем
+		if (arr[i] < arr[j]) {
+			// добавляем меньший элемент в массив для отсортированных элементов
+			buf[k++] = arr[i];
+			i++;
+		} else {
+			buf[k++] = arr[j];
+			j++;
+		}
+	}
+	// пока в первой половине есть элементы, добавляем их
+	while (i <= mid) {
+		buf[k++] = arr[i++];
+	}
+	// пока во второй половине есть элементы, добавляем их
+	while (j <= high) {
+		buf[k++] = arr[j++];
+	}
+	
+	// заменяем в исходном массиве отсортированным блоком элементов исходный блок
+	for (int i = 0; i < k; i++) {
+		arr[low++] = buf[i];
+	}	
+}
